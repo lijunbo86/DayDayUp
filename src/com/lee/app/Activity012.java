c056ba6fe058743a49237bbc871230741e414e72
William Lee <lijunbo86@126.com>
2014-09-16 15:51:28 +0800

    怀念那份温馨

---
+++ src/com/lee/app/Activity012.java
@@ -0,0 +1,15 @@
+package com.lee.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Activity012 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity012);
+	}
+}
